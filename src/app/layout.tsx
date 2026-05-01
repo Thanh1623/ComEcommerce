f6301f5f4a5562314642188efd47839ad9feba28
thanhnd <nguyenduythanh1623@gmail.com>
2026-05-01 12:07:51 +0700

    style: hide browser scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,9 +28,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased overflow-y-scroll`}
     >
-      <body className="min-h-screen flex flex-col">
+      <body className="min-h-full flex flex-col">
         <CartProvider>
           <Navbar />
           <main className="flex-grow">{children}</main>
